fff6f737bf823fe3c882acf8dd2741348ead18ff
MaathDEV <methidezeeni@gmail.com>
2017-07-11 22:56:19 -0300

    arrumado erro do command e colocado pixels do jimp

--- src/Jimp/Commands.js
+++ src/Jimp/Commands.js
@@ -6,14 +6,14 @@ var mask = require('./Mask.js');
 
 module.exports = {
   drake: function(event, client){
-    if(command.command('drake', event)){
+    if(cmd.command.command('drake', event)){
       // https://i.redd.it/cijkbpfu7eyy.jpg
       Jimp.read(event.author.avatarURL, function (err, image) {
-        image.resize(30, 30);
+        image.resize(150, 150);
         Jimp.read('https://i.redd.it/cijkbpfu7eyy.jpg').then(function (lenna) {
             lenna.resize(600, 600)
                  .composite(lenna, 0, 0)
-                 .composite(image, 30, 30)
+                 .composite(image, 300, 0)
                  .getBuffer(Jimp.MIME_PNG, (error, buffer) => {
                    event.channel.sendFile(buffer, 'teste.png')
                  });
