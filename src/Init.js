4a6c026bee74443d41e29541d93a2f16c4dc73f5
MaathDEV <methidezeeni@gmail.com>
2017-07-11 22:53:55 -0300

    Arrumado e adicionado comando drake

--- src/Init.js
+++ src/Init.js
@@ -1,9 +1,11 @@
 var vars = require('./Vars.js');
 var command = require('./Commands.js');
 var eval = require('./Eval.js');
+var jimp = require('./Jimp/Commands.js');
 
 module.exports = {
   init: function(event, client){
     eval.evalCommand(event, client);
+    jimp.drake(event, client);
   }
 }
